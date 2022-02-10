caaba0e9d106869f7edff62e2bc175bf8228dca8
yeeun_An <an446@naver.com>
2022-02-10 15:25:20 +0900

    ✨ localstorage key 값 수정

--- simple-note-app/js/main.js
+++ simple-note-app/js/main.js
@@ -14,48 +14,46 @@ form.addEventListener('keyup',() => {
   }
 })
 
-function save(value) {
-  let num = localStorage.length
-  num += 1; 
-  localStorage.setItem(`note`+num, value)
-}
 
 btn_add.addEventListener('click',() => {
    const save_val = textarea.value;
-   save(save_val);
+   const date = new Date().toLocaleString();
+   localStorage.setItem(date, save_val)
 })
 
 const container = document.querySelector('.cont-note');
 function print(){
   for (var i = 0; i < localStorage.length; i++){
+    const key = localStorage.key(i)
     console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
     const note_val = localStorage.getItem(localStorage.key(i))
     const div = document.createElement("div");
      div.classList.add("text");
-     div.classList.add(`note`+i);
      div.innerHTML=`
      <div>
         ${note_val}
       </div>
       <img src="../img/remove_circle_outline_black_24dp.svg" alt="" class="remove">
      `
-     document.querySelector(".cont-note").appendChild(div);
-     ["remove"].forEach((cls, p) => {
-       console.log(localStorage.key(p))
-      div
-        .querySelector(`.${cls}`)
-        .addEventListener("click", () => {
-          remove(localStorage.key(p));
-          
+     container.appendChild(div);
+     ["remove"].forEach((cls) => {
+       console.log(i)
+       div.querySelector(`.${cls}`)
+       .addEventListener("click", () => { remove(key)
         })
       });
-  }
-
+    }
 }
 print()
 
 function remove(key){
   localStorage.removeItem(key);
+  location.reload()
 }
 
-//문제1 : 메모를 remove하고 새로운 메모를 작성할 때, 키 값이 겹쳐서 overwright된다. ( length + 1 로 들어가기 때문) -> 내일까지 해결해보도록 하쟈!
+
+
+
+//문제1 : 메모를 remove하고 새로운 메모를 작성할 때, 키 값이 겹쳐서 overwright된다. ( length + 1 로 들어가기 때문) -> date로 key를 잡아서 해결함!
+
+//문제2 : reload으로 하니 부자연스럽다. display none으로 해서 유저 경험이 증가했으면 좋겠다. 
